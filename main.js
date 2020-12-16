dc9b2fbc8c36e92f9bc59460ec1bb8d25fe4f802
readwalkerchang <readwalkerchang@gmail.com>
2020-12-16 17:25:47 +0100

    test: replace img with Image src Property

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const displayedImage = document.querySelector('.displayed-img');
+let displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+let attribute = displayedImage.getAttribute("src");
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
 alert("Hello! I am an alert box!!");
 /* Wiring up the Darken/Lighten button */
+
+
+//testing code
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
+    document.querySelector('.displayed-img').src = 'images/pic2.jpg';
   }
