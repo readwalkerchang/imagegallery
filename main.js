7ccae121bacedce3ab15a712a59049ddfb3797ce
readwalkerchang <readwalkerchang@gmail.com>
2020-12-16 17:06:27 +0100

    test: DOM operation

--- main.js
+++ main.js
@@ -9,5 +9,8 @@ const overlay = document.querySelector('.overlay');
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
-
+alert("Hello! I am an alert box!!");
 /* Wiring up the Darken/Lighten button */
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
