5db058e1152cc76d72164c8bbbcd3f5c2b9b10d8
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2020-12-18 15:22:11 +0100

    feat: clickImg method

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-let displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
-
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-
 /* Looping through images */
-window.addEventListener('load', (event) => {
+
+function loopingImg(){
   for(let i = 1; i < 6; i++){
-    let item = document.createElement('img');
-    let image = 'images/pic'+ i+ '.jpg' ;
-    item.src = image;
-    let textnode = document.createTextNode("");//you have to create empty node
-    item.appendChild(textnode);
-    document.querySelector('.thumb-bar').appendChild(item);
+    let newImage = document.createElement('img');
+    let image = 'images/pic'+ i + '.jpg' ;//replace the image name in each iteration
+    newImage.setAttribute('src', image);
+    document.querySelector('.thumb-bar').appendChild(newImage); //declare DOM here
   }
+}
+
+
+// If you have ES6 support you can replace your last line with:
+
+function clickImg(){
+  function myFunction(){
+    var attribute = this.getAttribute("src");
+    let displayedImage = document.querySelector('.displayed-img');
+    displayedImage.setAttribute('src', attribute);
+  }
+  var elements = document.querySelectorAll("img");
+  Array.from(elements).forEach(function(element) {
+    element.addEventListener('click', myFunction);
+  });
+}
+
+
+
+window.addEventListener('load', (event) => {
+  loopingImg()
+  clickImg()
 });
 
 
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
 
 
 //testing code
 function myFunction() {
-    for(let i = 1; i < 6; i++){
-      let item = document.createElement('img');
-      let image = 'images/pic'+ i+ '.jpg' ;
-      item.src = image;
-      let textnode = document.createTextNode("");//you have to create empty node
-      item.appendChild(textnode);
-      document.getElementById('test').appendChild(item);
-    }
+
   }
